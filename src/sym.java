86af4e83cfeee041d9170e483be232c369360f11
Iustin <sibiescu_iustin@yahoo.com>
2015-03-03 17:32:53 +0000

    seq reduced

--- src/sym.java
+++ src/sym.java
@@ -6,63 +6,62 @@
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int FORALL = 43;
-  public static final int DIVIDE = 22;
-  public static final int MAIN_RETURN = 55;
-  public static final int LTE = 26;
-  public static final int LBRACKET = 49;
-  public static final int LPAREN = 17;
-  public static final int SEMI = 32;
-  public static final int MINUS = 20;
-  public static final int RPAREN = 18;
-  public static final int LCURL = 51;
-  public static final int NOT = 31;
-  public static final int AND = 29;
-  public static final int LT = 25;
-  public static final int TDEF = 57;
-  public static final int IN = 44;
-  public static final int OR = 30;
-  public static final int COMMA = 53;
-  public static final int FI = 37;
+  public static final int FORALL = 42;
+  public static final int DIVIDE = 21;
+  public static final int MAIN_RETURN = 54;
+  public static final int LTE = 25;
+  public static final int LBRACKET = 48;
+  public static final int LPAREN = 16;
+  public static final int SEMI = 31;
+  public static final int MINUS = 19;
+  public static final int RPAREN = 17;
+  public static final int LCURL = 50;
+  public static final int NOT = 30;
+  public static final int AND = 28;
+  public static final int LT = 24;
+  public static final int TDEF = 56;
+  public static final int IN = 43;
+  public static final int OR = 29;
+  public static final int COMMA = 52;
+  public static final int FI = 36;
   public static final int FLOAT_NUMBER = 13;
-  public static final int PLUS = 19;
-  public static final int MAIN = 48;
-  public static final int IF = 36;
-  public static final int ASSIGN = 35;
-  public static final int DOT = 34;
+  public static final int PLUS = 18;
+  public static final int MAIN = 47;
+  public static final int IF = 35;
+  public static final int ASSIGN = 34;
+  public static final int DOT = 33;
   public static final int ID = 2;
   public static final int EOF = 0;
-  public static final int RETURN = 54;
-  public static final int OD = 42;
-  public static final int RBRACKET = 50;
+  public static final int RETURN = 53;
+  public static final int OD = 41;
+  public static final int RBRACKET = 49;
   public static final int error = 1;
   public static final int DICT_TYPE = 8;
-  public static final int NEQ = 28;
-  public static final int FDEF = 56;
+  public static final int NEQ = 27;
+  public static final int FDEF = 55;
   public static final int INTEGER_TYPE = 5;
-  public static final int EQ = 27;
+  public static final int EQ = 26;
   public static final int CHARACTER_TYPE = 4;
-  public static final int GTE = 24;
-  public static final int RCURL = 52;
+  public static final int GTE = 23;
+  public static final int RCURL = 51;
   public static final int INTEGER_NUMBER = 12;
-  public static final int TIMES = 21;
+  public static final int TIMES = 20;
   public static final int BOOLEAN_VALUE = 10;
-  public static final int COLON = 33;
-  public static final int ELSE = 39;
-  public static final int STRING_VALUE = 16;
-  public static final int READ = 45;
-  public static final int WHILE = 40;
+  public static final int COLON = 32;
+  public static final int ELSE = 38;
+  public static final int STRING_VALUE = 15;
+  public static final int READ = 44;
+  public static final int WHILE = 39;
   public static final int SEQ_TYPE = 9;
-  public static final int THEN = 38;
+  public static final int THEN = 37;
   public static final int RATIONAL_TYPE = 6;
   public static final int CHARACTER_VALUE = 11;
-  public static final int SEQ_VALUE = 14;
-  public static final int ALIAS = 47;
-  public static final int RATIONAL_NUMBER = 15;
-  public static final int GT = 23;
+  public static final int ALIAS = 46;
+  public static final int RATIONAL_NUMBER = 14;
+  public static final int GT = 22;
   public static final int FLOAT_TYPE = 7;
-  public static final int PRINT = 46;
-  public static final int DO = 41;
+  public static final int PRINT = 45;
+  public static final int DO = 40;
   public static final int BOOLEAN_TYPE = 3;
   public static final String[] terminalNames = new String[] {
   "EOF",
@@ -79,7 +78,6 @@ public class sym {
   "CHARACTER_VALUE",
   "INTEGER_NUMBER",
   "FLOAT_NUMBER",
-  "SEQ_VALUE",
   "RATIONAL_NUMBER",
   "STRING_VALUE",
   "LPAREN",
--- tests/open/iustin.s
+++ tests/open/iustin.s
@@ -3,8 +3,8 @@ main {
 	alias seq<char> string;
 	alias fred spud;
 	tdef person { name:int, jk:string, sadfsadf:safdasdf };
-	a : seq<int> := ["aSASD","assaf"];
-	a : dict<int,sads> := {1:'a', 1:["asda","asfaf"]};
+	a : seq<int> := ["aSASD",[1,2,3,"dsa",[]],{"sa":3},ds[1:2]];
+	a : dict<int,sads> := {1:'a', 1:["asda","asfaf"],f(a):ds[1:2], 2:{}};
 	return;
 };
 
